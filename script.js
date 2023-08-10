f015cab0660a5e65357c2ac8a8310713d3df8121
Thiago Silva <tkreserva@gmail.com>
2023-08-10 16:56:01 -0300

    Organizacao dos icones dos produtos, bloqueio de imagens para não ser arrastadas, etc.

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
-const prods = [
+//Array de Produtos, que pode ser substituido por um 'Bando de dados' futuramente
+const prods=[
     {
-        'titulo': "Garrafinha",
-        'quantidade': "250ml",
-        'embalagem': "Embalagem de PET",
-        'descricao': "Perfeito para a garotada!",
-        'img': './src/produto_garafinha.png'
+    'titulo':"Garrafinha",
+    'quantidade':"250ml",
+    'embalagem':"Embalagem de PET",
+    'descricao':"Perfeito para a garotada!",
+    'img':'./src/produto_garafinha.png'
     },
     {
-        'titulo': "Garrafa",
-        'quantidade': "2L",
-        'embalagem': "Embalagem de PET",
-        'descricao': "A melhor opção para seus encontros, seja com sua família ou amigos!",
-        'img': './src/produto_garrafa.png'
+    'titulo':"Garrafa",
+    'quantidade':"2L",
+    'embalagem':"Embalagem de PET",
+    'descricao':"A melhor opção para seus encontros, seja com sua família ou amigos!",
+    'img':'./src/produto_garrafa.png'
     },
     {
-        'titulo': "Latinha",
-        'quantidade': "350ml",
-        'embalagem': "Embalagem de Alumínio",
-        'descricao': "Uma ótima escolha para os  dias quentes.",
-        'img': './src/produto_lata.png'
+    'titulo':"Latinha",
+    'quantidade':"350ml",
+    'embalagem':"Embalagem de Alumínio",
+    'descricao':"Uma ótima escolha para os dias quentes.",
+    'img':'./src/produto_lata.png'
     },
     {
-        'titulo': "Picolé",
-        'quantidade': "60g",
-        'embalagem': "Embalagem de Plástico",
-        'descricao': "Com um sabor unico que só a São Geraldo possui",
-        'img': './src/produto_picole.png'
+    'titulo':"Picolé",
+    'quantidade':"60g",
+    'embalagem':"Embalagem de Plástico",
+    'descricao':"Com um sabor unico que só a São Geraldo possui.",
+    'img':'./src/produto_picole.png'
     },
     {
-        'titulo': "Garrafa",
-        'quantidade': "200ml",
-        'embalagem': "Embalagem de Vidro",
-        'descricao': "A novidade saborosa da São Geraldo!",
-        'img': './src/produto_vidro.png'
+    'titulo':"Garrafa",
+    'quantidade':"200ml",
+    'embalagem':"Embalagem de Vidro",
+    'descricao':"A novidade saborosa da São Geraldo!",
+    'img':'./src/produto_vidro.png'
     }
-]
-
-const titulo = document.getElementsByClassName('texto_prod');
-function Prod(i) {
-    i = i - 1;
-    titulo[0].getElementsByTagName('h2')[0].innerText = prods[i].titulo;
-    titulo[0].getElementsByTagName('p')[0].innerText = prods[i].quantidade;
-    titulo[1].getElementsByTagName('h2')[0].innerText = prods[i].embalagem;
-    titulo[1].getElementsByTagName('p')[0].innerText = prods[i].descricao;
-    document.getElementById("img_prod").src = prods[i].img;
-}
-
-function Inicio() {
-    titulo[0].getElementsByTagName('h2')[0].innerText = prods[0].titulo;
-    titulo[0].getElementsByTagName('p')[0].innerText = prods[0].quantidade;
-    titulo[1].getElementsByTagName('h2')[0].innerText = prods[0].embalagem;
-    titulo[1].getElementsByTagName('p')[0].innerText = prods[0].descricao;
-    document.getElementById("img_prod").src = prods[0].img;
-}
-Prod(1)
+    ]
+    //Preparando para receber dados dos produtos
+    const titulo=document.getElementsByClassName('texto_prod');
+    //Ao clicar em um icone do produto as informações sobre ele é carregada
+    function Prod(i){
+    i=i-1;
+    titulo[0].getElementsByTagName('h2')[0].innerText=prods[i].titulo;
+    titulo[0].getElementsByTagName('p')[0].innerText=prods[i].quantidade;
+    titulo[1].getElementsByTagName('h2')[0].innerText=prods[i].embalagem;
+    titulo[1].getElementsByTagName('p')[0].innerText=prods[i].descricao;
+    document.getElementById("img_prod").src=prods[i].img;
+    }
+    //Carrega o primeiro item, para não deixar vazio
+    function Inicio(){
+    titulo[0].getElementsByTagName('h2')[0].innerText=prods[0].titulo;
+    titulo[0].getElementsByTagName('p')[0].innerText=prods[0].quantidade;
+    titulo[1].getElementsByTagName('h2')[0].innerText=prods[0].embalagem;
+    titulo[1].getElementsByTagName('p')[0].innerText=prods[0].descricao;
+    document.getElementById("img_prod").src=prods[0].img;
+    }
+    //Efeito de ampliação dos icones dos produtos
+    var imgs=document.getElementsByClassName("prod_imgs");
+    for(let i=0;i<imgs.length;i++){
+    imgs[i].addEventListener("mouseover",function(){imgs[i].style.transform="scale(1.4)";});
+    imgs[i].addEventListener("mouseout",function(){imgs[i].style.transform="scale(1.0)";});
+    }
