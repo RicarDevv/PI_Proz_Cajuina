eafdf6c83a6ffa67b42cb5bfe2bc963fef638dd4
Ricardo Gabriel <91565135+RicarDevv@users.noreply.github.com>
2023-08-21 23:38:34 -0300

    21_08Update
    Update 21/08

--- script.js
+++ script.js
@@ -59,3 +59,134 @@ for(let i=0;i<imgs.length;i++){
 imgs[i].addEventListener("mouseover",function(){imgs[i].style.transform="scale(1.4)";});
 imgs[i].addEventListener("mouseout",function(){imgs[i].style.transform="scale(1.0)";});
 }
+//Validação de formulário
+let usernameInput = document.getElementById("nomesobrenome");
+let usernameLabel = document.querySelector('label[for="nomesobrenome"]');
+let usernameHelper = document.getElementById("username-helper");
+
+//Mostrar popup - Campo Obrigatório
+usernameInput.addEventListener('focus', () => {
+    usernameLabel.classList.add('required-popup')
+})
+
+//Remover popup - Campo Obrigatório
+usernameInput.addEventListener('blur', (event) => {
+    usernameLabel.classList.remove('required-popup')
+})
+
+//Validação do valor da input
+function validationInputEmail(input, inputValue ){
+
+    if (inputValue.includes('@') && inputValue.includes('.com')) {
+                emailHelper.classList.remove('visible')
+                input.classList.remove('error')
+                input.classList.add('correct')
+            } else {
+                input.classList.remove('correct')
+                input.classList.add('error')
+                emailHelper.classList.add('visible')
+            }
+}
+
+function inputValidation(input){
+    input.addEventListener('blur', (event) => {
+        const inputValue = event.target.value
+
+        if(event.target.id === 'email'){
+            validationInputEmail(input, inputValue)
+        }  else {
+
+            if(inputValue.length < 4) {
+                input.classList.remove('correct')
+                input.classList.add('error')
+                usernameHelper.classList.add('visible')
+            } else {
+                input.classList.remove('error')
+                usernameLabel.classList.remove('required-popup')
+                input.classList.add('correct')
+                usernameHelper.classList.remove('visible')
+            }
+         }   
+    })
+}
+
+
+inputValidation(usernameInput)
+
+const emailInput = document.getElementById('email')
+let emailLabel = document.querySelector('label[for="email"]');
+let emailHelper = document.getElementById("email-helper");
+
+emailInput.addEventListener('focus', () => {
+    emailLabel.classList.add('required-popups')
+})
+
+emailInput.addEventListener('blur', (event) => {
+    emailLabel.classList.remove('required-popups')
+})
+
+inputValidation(emailInput)
+
+
+const telefoneInput = document.getElementById('telefone')
+let telefoneLabel = document.querySelector('label[for="telefone"]');
+let telefoneHelper = document.getElementById("telefone-helper");
+
+telefoneInput.addEventListener('focus', () => {
+    telefoneLabel.classList.add('required-popups')
+})
+
+telefoneInput.addEventListener('blur', (event) => {
+    telefoneLabel.classList.remove('required-popups')
+})
+window.onload = function(){
+    var entidade = document.getElementById('imagem');
+  
+    var fator_aumento = 1.5; // Fator de aumento de tamanho
+    var tamanho_original = entidade.clientWidth; // Tamanho original da imagem
+  
+    var aumentada = false; // Variável para controlar o estado da imagem
+  
+    entidade.onclick = function () {
+      if (aumentada) {
+        // Reduz a imagem para o tamanho original
+        this.style.width = tamanho_original + "px";
+      } else {
+        // Aumenta a imagem
+        this.style.width = (tamanho_original * fator_aumento) + "px";
+      }
+      
+      // Alterna o estado
+      aumentada = !aumentada;
+    };
+  }
+  
+const showMoreLink = document.querySelector('.show-more');
+        const showLessLink = document.querySelector('.show-less');
+        const moreContent = document.querySelector('.more-content');
+
+        showMoreLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            moreContent.style.display = 'block';
+            showMoreLink.style.display = 'none';
+            showLessLink.style.display = 'inline';
+        });
+
+        showLessLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            moreContent.style.display = 'none';
+            showMoreLink.style.display = 'inline';
+            showLessLink.style.display = 'none';
+        });
+
+        function openModal(imageSrc) {
+            var modal = document.getElementById("myModal");
+            var modalImg = document.getElementById("modalImg");
+            modal.style.display = "block";
+            modalImg.src = imageSrc;
+        }
+        
+        function closeModal() {
+            var modal = document.getElementById("myModal");
+            modal.style.display = "none";
+        }
